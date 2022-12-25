ef338b4d980399c0ecbdc2071a22c37adbefb919
Mehmet TAS <101858286+mehmettas1@users.noreply.github.com>
2022-12-25 13:30:21 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,535 @@
+const linkData = [
+    {
+      id: 1,
+      linkAdi: 'Teknoloji ',
+      altLinkler: [
+        {
+          id: 1,
+          acilirLink: 'TV',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'Samsung',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'LG',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'Arçelik',
+            },
+            {
+              id: 4,
+              enAltLinkAdi: 'beko',
+            },
+            {
+              id: 4,
+              enAltLinkAdi: 'keko',
+            },
+          ],
+        },
+        {
+          id: 2,
+          acilirLink: 'Bilgisayar',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'Lenova',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'Asus',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'HP',
+            },
+          ],
+        },
+        {
+          id: 3,
+          acilirLink: 'Telefon',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: ' Apple 12',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'Apple 13',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'Apple 14',
+            },
+          ],
+        },
+        {
+          id: 4,
+          acilirLink: 'Buzdolabı',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'Arçelik',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'Beko',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'Vestel',
+            },
+          ],
+        },
+        {
+          id: 4,
+          acilirLink: 'TV',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'Samsung',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'LG',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'Arçelik',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 1,
+      linkAdi: 'Yemek',
+      altLinkler: [
+        {
+          id: 1,
+          acilirLink: 'Izgara',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'Adana',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'Urfa',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'ÇiğerŞiş',
+            },
+          ],
+        },
+        {
+          id: 2,
+          acilirLink: 'Sulu Yemek',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'Kuru fasulye',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'Bamya',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'Patates',
+            },
+          ],
+        },
+        {
+          id: 3,
+          acilirLink: 'Tatlı',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'Sütlaç ',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'Kazandibi',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'Baklava',
+            },
+          ],
+        },
+        {
+          id: 4,
+          acilirLink: 'Çorba',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'Mercimek',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'Paça',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'İşkembe',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 1,
+      linkAdi: 'Link3',
+      altLinkler: [
+        {
+          id: 1,
+          acilirLink: 'BirinciLink1',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 2,
+          acilirLink: 'BirinciLink2',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '2İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '2İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '2İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 3,
+          acilirLink: 'BirinciLink3',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '3İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '3İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '3İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 4,
+          acilirLink: 'BirinciLink4',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '4İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '4İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '4İkinciLink3',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 1,
+      linkAdi: 'Link4',
+      altLinkler: [
+        {
+          id: 1,
+          acilirLink: 'BirinciLink1',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 2,
+          acilirLink: 'BirinciLink2',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '2İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '2İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '2İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 3,
+          acilirLink: 'BirinciLink3',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '3İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '3İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '3İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 4,
+          acilirLink: 'BirinciLink4',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '4İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '4İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '4İkinciLink3',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 1,
+      linkAdi: 'Link5',
+      altLinkler: [
+        {
+          id: 1,
+          acilirLink: 'BirinciLink1',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: 'İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: 'İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: 'İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 2,
+          acilirLink: 'BirinciLink2',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '2İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '2İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '2İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 3,
+          acilirLink: 'BirinciLink3',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '3İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '3İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '3İkinciLink3',
+            },
+          ],
+        },
+        {
+          id: 4,
+          acilirLink: 'BirinciLink4',
+          enAltLinkler: [
+            {
+              id: 1,
+              enAltLinkAdi: '4İkinciLink1',
+            },
+            {
+              id: 2,
+              enAltLinkAdi: '4İkinciLink2',
+            },
+            {
+              id: 3,
+              enAltLinkAdi: '4İkinciLink3',
+            },
+          ],
+        },
+      ],
+    },
+  ]
+
+
+const firstUl =document.querySelector(".first-layer-ul");
+
+const svg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+</svg>
+`;
+
+
+linkData.forEach((element)=>{
+const li1=document.createElement("li");
+li1.setAttribute("class"," cursor-pointer first-layer-li pl-3 flex justify-between   ");
+li1.innerHTML=element.linkAdi;
+li1.innerHTML +=svg;
+
+firstUl.appendChild(li1);
+
+const ul2 = document.createElement("ul");
+ul2.setAttribute("class","absolute hidden flex flex-col left-[508px] top-[0] ml-1  second-layer-ul ");
+li1.appendChild(ul2);
+
+
+
+element.altLinkler.forEach((el)=>{
+const li2=document.createElement("li");
+li2.setAttribute("class","bg-gray-400 cursor-pointer w-[508px] h-[57px] second-layer-li flex justify-between py-[18px]");
+li2.innerHTML=el.acilirLink;
+li2.innerHTML +=svg;
+ul2.appendChild(li2);
+
+
+// 3katman
+const ul3 = document.createElement("ul");
+ul3.setAttribute("class","absolute hidden  flex flex-col left-[510px]  top-0 ml-1 third-layer-ul");
+li2.appendChild(ul3);
+
+el.enAltLinkler.forEach((ele)=>{
+    const li3=document.createElement("li");
+    li3.setAttribute("class","bg-gray-400 cursor-pointer w-[510px] h-[57px] flex justify-between   third-layer-li py-[18px] pl-1");
+    li3.innerHTML=ele.enAltLinkAdi;
+    li3.innerHTML +=svg;
+    ul3.appendChild(li3);
+})
+
+
+})
+
+
+})
+
+// fonksiyon
+const firstLi =document.querySelectorAll('.first-layer-li')
+const secondUl =document.querySelector(".second-layer-ul");
+const secondLi =document.querySelectorAll(".second-layer-li");
+const thirdUl =document.querySelectorAll(".third-layer-ul");
+const thirdLi =document.querySelectorAll(".third-layer-li");
+
+firstLi.forEach((element)=>{
+    element.addEventListener('click',(item)=>{
+      
+      if (item.target.querySelector(".second-layer-ul")) {
+        document.querySelectorAll(".second-layer-ul").forEach((el)=>{
+            el.style.display = 'none'
+        })
+        document.querySelectorAll(".third-layer-ul").forEach((el)=>{
+          el.style.display = 'none'
+      })
+        
+          
+        }
+        if (item.target.querySelector(".second-layer-ul")) {
+          item.target.querySelector(".second-layer-ul").style.display = 'block';
+          
+        }
+
+        
+        
+
+       item.target.querySelector(".second-layer-ul ")?.addEventListener('click',(li)=>{
+
+         if (li.target.querySelector(".third-layer-ul")) {
+          document.querySelectorAll(".third-layer-ul").forEach((el)=>{
+            el.style.display="none"
+          })
+         }
+        if(li.target.querySelector(".third-layer-ul")){
+           li.target.querySelector(".third-layer-ul").style.display ="block";
+         }
+        })
+
+    
+    })
+    
+})
+
+document.querySelectorAll(".third-layer-li").forEach((el)=>{
+  el.addEventListener("click",(el)=>{
+    console.log(el.target.innerHTML);
+  })
+})
+// console.log(secondLi);
+
+// kapatma şekli
+document.addEventListener('click',(event)=>{
+// console.log(event.target);
+if (!event.target.closest(".first-layer-ul")) {
+  console.log("meta");
+  document.querySelectorAll(".second-layer-ul").forEach((el)=>{
+el.style.display="none";
+  })
+
+  document.querySelectorAll(".third-layer-ul").forEach((el)=>{
+    el.style.display="none";
+      })
+}
+})
